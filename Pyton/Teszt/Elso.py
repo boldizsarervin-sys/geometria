54cd63a7e4e19d4cbc1b0adca6df8c55eff07284
boldizsarervin-sys <boldizsar.ervin@gmail.com>
2026-06-10 09:32:55 +0200

    tisztitas3

--- Pyton/Teszt/Elso.py
+++
@@ -1,6 +0,0 @@
-#Ez ale első próbalkozás
-
-nev = input ("Mi a neved? ")
-print ("Helló " + nev + "!")
-print ("Gratulálok az első Pyton programodhoz!")
-input ("Nyomj Enter-t a kilépéshez ...") # Ez tartja nyitva az ablakot
